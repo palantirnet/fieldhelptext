a347867f73d7a24812fa7192b36b6e8720b6905c
Bec White <white@palantir.net>
2020-01-03 12:59:19 -0600

    Add a field name param converter.

---
+++ src/ParamConverter/FieldNameConverter.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @copyright Copyright 2020 Palantir.net, Inc.
+ */
+
+namespace Drupal\fieldhelptext\ParamConverter;
+
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\ParamConverter\ParamConverterInterface;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Route parameter converter for field names to field configs.
+ */
+class FieldNameConverter implements ParamConverterInterface {
+
+  /**
+   * @var EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
+  /**
+   * Constructs a new EntityConverter.
+   */
+  public function __construct(EntityFieldManagerInterface $entity_field_manager) {
+    $this->entityFieldManager = $entity_field_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convert($value, $definition, $name, array $defaults) {
+    /** @var EntityTypeInterface $entity_type */
+    $entity_type = array_reduce($defaults, function($carry, $item) { return is_a($item, '\Drupal\Core\Entity\EntityTypeInterface') ? $item : $carry; });
+
+    $fields = $this->entityFieldManager->getFieldStorageDefinitions($entity_type->id());
+
+    return isset($fields[$value]) ? $value : NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies($definition, $name, Route $route) {
+    return (!empty($definition['type']) && $definition['type'] === 'field_name');
+  }
+
+}
